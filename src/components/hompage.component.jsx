09f5ce0300079718dbd1208ce833fed3eaea6800
shubhamkarki <shubhamkarkiworkings@gmail.com>
2019-11-20 12:14:58 +0530

    create project with create react appp

---
+++ src/components/hompage.component.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+export const Hompage = () => {
+    return (
+        <div className = 'hompage' >
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Hats</h1>
+                    <span className="subtitle">shop now</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Hats</h1>
+                    <span className="subtitle">shop now</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Hats</h1>
+                    <span className="subtitle">shop now</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Hats</h1>
+                    <span className="subtitle">shop now</span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Hats</h1>
+                    <span className="subtitle">shop now</span>
+                </div>
+            </div>
+        </div>
+            
+        </div>
+    )
+}
