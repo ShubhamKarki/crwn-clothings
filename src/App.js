6e1996e4d8e979f4887af09839bf571d4f127e5e
shubhamkarki <shubhamkarkiworkings@gmail.com>
2019-11-22 00:04:42 +0530

    added checkout page, checkout item component and wpdated cart to use selectors from reselect

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import Homepage  from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from  './redux/user/user.action';
-
-
 
 class App extends React.Component {
   
@@ -47,6 +47,7 @@ class App extends React.Component {
           <Switch>
         <Route exact path='/' component={Homepage}/>
         <Route  path='/shop' component={ShopPage}/>
+        <Route exact path='/checkout' component={CheckoutPage}/>
         <Route
             exact
             path='/signin'
